80a3e7c02d781ddc82aff9d46ef430bc3fc40a4f
bangroy26264 <112047927+bangroy26264@users.noreply.github.com>
2022-08-25 13:44:34 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+    <link href="../coba/style.css" rel="stylesheet" />
+    </head>
+    <form id="login" action="none">
+        <h1 id="ff-proof" class="ribbon">PT. Wina Wira Usaha Jaya &nbsp;&nbsp;</h1>
+        <div class="apple">
+        <div class="top"><span></span></div>
+        <div class="butt"><span></span></div>
+        <div class="bite"></div>
+    </div>
+        <fieldset id="inputs">
+            <input id="username" type="text" onblur="if(this.value=='')this.value='User ID';" onfocus="if(this.value=='User ID')this.value='';" value="User ID" />
+            <input id="password" type="text" onblur="if(this.value=='')this.value='Password';" onfocus="if(this.value=='Password')this.value='';" value="Password" />
+        </fieldset>
+        <fieldset id="actions">
+            <button><a href="../coba/form.php">Log In</button>
+            <!-- <input type="submit" id="submit" value="Log In"/> -->
+        <p class="option"><a href="#">New Akun</a>&nbsp;&nbsp;|<a href="#">Forgot Password</a></p>
+        </fieldset>
+    </form>
+    
+    <script src="../coba/java.js"></script>
+</html>
